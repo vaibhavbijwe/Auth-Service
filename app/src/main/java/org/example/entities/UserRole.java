6bb8dca5c009408c6bdea67240813c808153f8aa
Vaibhav Bijwe <vaibhavbijwe30@gmail.com>
2025-07-23 22:05:33 +0530

    initial

---
+++ app/src/main/java/org/example/entities/UserRole.java
@@ -0,0 +1,31 @@
+package org.example.entities;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
+@Entity
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class UserRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private Long roleId;
+
+    private String name;
+
+}
